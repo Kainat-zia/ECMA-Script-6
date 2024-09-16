eb131d3d8c506852f8900c6e93cc77e92971086b
Kainat zia <154695099+Kainat-zia@users.noreply.github.com>
2024-09-16 15:53:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -80,4 +80,27 @@ let promise = new Promise((resolve, reject) =>{
     console.log("this is a promise");
     reject("some errors caught")
  })
- 
+
+
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
+}
+
+let myPromise = new Promise(function(myResolve, myReject) {
+  let x = 0;
+
+// The producing code (this may take some time)
+
+  if (x == 0) {
+    myResolve("OK");
+  } else {
+    myReject("Error");
+  }
+});
+
+myPromise.then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
+
+//  value bydefault hota h
